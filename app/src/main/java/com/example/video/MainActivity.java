f1085c6fe7d940f82bcfecc6c1ffaa7c9d9516ad
omkar <mkrsarde@gmail.com>
2024-03-14 22:01:42 +0530

    this is video project

---
+++ app/src/main/java/com/example/video/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.video;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,10);
+                startActivityForResult(intent,1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            VideoView vedioview = new VideoView(this);
+            vedioview.setVideoURI(data.getData());
+            vedioview.start();
+            builder.setView(vedioview).show();
+        }
+    }
+}
